979b56386d8d926fa26f84b87ddda272f64c2b7b
hackyoung <hackyoung@163.com>
2018-08-27 11:38:17 +0800

    fff

--- helper.go
+++ helper.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"bytes"
 	"math/rand"
+	"reflect"
 	"regexp"
 )
 
@@ -108,4 +109,46 @@ func RandTemplateString(template string, length int) string {
 		b[i] = runes[rand.Intn(ler)]
 	}
 	return string(b)
+}
+
+func InstanceOf(v interface{}, t interface{}) bool {
+	if v == nil || t == nil {
+		return false
+	}
+	tt := reflect.TypeOf(t)
+	tv := reflect.TypeOf(v)
+
+	if tt.Kind() == reflect.Ptr {
+		tt = tt.Elem()
+	}
+	if tv.Kind() == reflect.Ptr {
+		tv = tv.Elem()
+	}
+
+	if tt.Kind() == reflect.Interface {
+		if tv.Kind() != reflect.Interface {
+			return reflect.PtrTo(tv).Implements(tt)
+		} else {
+			return tv.Implements(tt)
+		}
+	} else {
+		return tt == tv || hasEmbedded(tv, tt)
+	}
+}
+
+func hasEmbedded(tv reflect.Type, tt reflect.Type) bool {
+	if tt.Kind() == reflect.Ptr {
+		tt = tt.Elem()
+	}
+	if tv.Kind() == reflect.Ptr {
+		tv = tv.Elem()
+	}
+
+	for i := 0; i < tv.NumField(); i++ {
+		tsf := tv.Field(i)
+		if tsf.Anonymous && (tt == tsf.Type || hasEmbedded(tsf.Type, tt)) {
+			return true
+		}
+	}
+	return false
 }
